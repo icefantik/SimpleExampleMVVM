ddd3fc5921a971b306cc41cd750327cec749a180
Petya <Petya@DESKTOP-9032-2>
2025-04-15 16:58:39 +0300

    first commit

---
+++ SimpleExampleMVVM/ViewModels/SendCommentViewModel.cs
@@ -0,0 +1,51 @@
+﻿using SimpleExampleMVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleExampleMVVM.ViewModels;
+
+public class SendCommentViewModel : INotifyPropertyChanged
+{
+    //private SendCommentModel _sendCommentModel;
+
+    private string? _comment;
+
+    public string Comment
+    {
+        get => _comment!;
+        set
+        {
+            if (_comment != value)
+            {
+                _comment = value;
+
+                RaisePropertyChanged();
+
+                RaisePropertyChanged(nameof(Comment));
+            }
+        }
+    }
+
+    //public SendCommentViewModel()
+    //{
+    //    _sendCommentModel = new SendCommentModel();
+    //}
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public void SendComment()
+    {
+        //_sendCommentModel.SendComment(Comment);
+    }
+}
---
+++ SimpleExampleMVVM/Views/SendCommentView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using SimpleExampleMVVM.ViewModels;
+using System.Windows;
+
+namespace SimpleExampleMVVM.Views;
+
+/// <summary>
+/// Interaction logic for SendCommentView.xaml
+/// </summary>
+public partial class SendCommentView : Window
+{
+    public SendCommentView()
+    {
+        InitializeComponent();
+
+        //DataContext = new SendCommentViewModel();
+    }
+}
